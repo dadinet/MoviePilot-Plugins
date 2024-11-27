16f11cb25f6d5ae9f4e1b7c20424ca21a7e2cb85
dadinet <131777059+dadinet@users.noreply.github.com>
2024-11-27 16:15:14 +0000

    123

--- README.md
+++ README.md
@@ -1,14 +1,6 @@
 # MoviePilot-Plugin-Market
 
-MoviePilot第三方插件市场：https://github.com/dadinet/MoviePilot-Plugins
-
-### 插件列表
-
-| 序号 | 名称 | 当前版本 | 功能简述 |
-|:--:|:--:|:--:|:--|
-| 1  | Telegram消息通知  | v1.4 | 支持5个Telegram接收消息通知。|
-
-### 短剧刮削并非v2版本 是v1只进行硬连接改名不尽兴刮削
+# 自用库
 
 
 
--- plugins.v2/shortplaymonitor/__init__.py
+++ plugins.v2/shortplaymonitor/__init__.py
@@ -60,7 +60,7 @@ class ShortPlayMonitor(_PluginBase):
     # 插件名称
     plugin_name = "短剧刮削"
     # 插件描述
-    plugin_desc = "监控视频短剧创建，刮削。"
+    plugin_desc = "个人修改使用"
     # 插件图标
     plugin_icon = "https://raw.githubusercontent.com/xushier/HD-Icons/refs/heads/main/border-radius/Amule_B.png"
     # 插件版本
